ded32f53b10fc95a71c85a354c0c9407abd75c6c
wikmor <wikmor@proton.me>
2025-08-05 09:43:17 +0200

    Remove /libraries folder check

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -10,8 +10,8 @@ def test_plugin_enabled():
     content = log_path.read_text(encoding="utf-8", errors="ignore")
 
     # Test if libraries folder which is in the main server's folder is not missing
-    libraries_path = pathlib.Path("/libraries")
-    assert libraries_path.exists(), "❌ Libraries folder does not exist"
+    # libraries_path = pathlib.Path("/libraries")
+    # assert libraries_path.exists(), "❌ Libraries folder does not exist"
 
     assert "Downloading library adventure-platform-bukkit-" in content, "❌ Adventure Platform Bukkit library not found in logs"
     assert "Downloading library adventure-api-" in content, "❌ Adventure API library not found in logs"
